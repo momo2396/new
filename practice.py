7426ddb7346ed95667871712f168c2fe337ea746
momo2396 <foyazunnesamomo.345@gmail.com>
2023-08-29 13:02:43 +0600

    first commit

---
+++ practice.py
@@ -0,0 +1,53 @@
+# import sympy
+# sympy.init_printing()
+# x =sympy.symbols('x')
+# exp = 2**x+1
+# print(exp)
+
+# import matplotlib as mpl
+# from matplotlib import axis
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # fig, ax = plt.subplots()  # Create a figure containing a single axes.
+# plt.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+# plt.xlabel("xLable")
+# plt.ylabel("yLable")
+# plt.title("mmmm")
+# plt.show()
+
+
+import sympy 
+from sympy import sqrt
+# sqrt(2**2)
+# sqrt((-2)**2)
+# x = 2
+# print(sqrt(x**2))
+# print(sqrt(x**2) == x)
+# y = -2
+# print(y)
+# sqrt(y**2) == y
+# sqrt(y**2) == -y
+
+# sympy.sin(2)
+# x, y= sympy.symbols('x y')
+# y = sympy.sin(x)
+# expr = sympy.series(y, x, n=10)
+# print(expr)
+
+# print(sympy.solve(x**2 - y, x, dict=True))
+
+import sympy as sp
+import math
+sp.init_printing()
+# print(sp.sqrt(2))
+# sp.sqrt(2)
+
+x, y= sympy.symbols('x y')
+
+print(2*x + x -3)
+ex = 2*x+1
+print(sp.sin(x)**2 + sp.cos(x)**2)
+
+print(2*x /6)
+display(ex)
